8695bebf0807cafc6338920b3ac9cdeb0bcf4e8c
Mr Jose Sanchez <leandrosanchez.it@gmail.com>
2022-12-18 00:32:12 -0400

    Added UI improvements

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-var solicitado_capitan = document.getElementById('solicitado_capitan');
+const solicitado_capitan = document.getElementById('solicitado_capitan');
 const contenido_avion = document.getElementById('contenido_avion');
 
 const btnCalcular = document.getElementById('btnCalcular');
@@ -8,18 +8,25 @@ const resultado = document.getElementById('resultado');
 const historico = document.getElementById('historico')
 const historial = document.getElementById('historial')
 
-var operaciones = [];
+let operaciones = [];
 const display = JSON.parse(localStorage.getItem('db_operaciones'))
 operaciones = display;
+
+class AirpcraftFuel {
+    constructor(fuelToPlane,fuelToWings){
+        this.fuelToPlane = fuelToPlane
+        this.fuelToWings = fuelToWings
+    }
+}
 
 const mostrarHistorico = () => {
     historico.innerHTML=''
     operaciones.map( (element,index) => {
         historico.innerHTML += `
-        <li class="list-group-item d-flex justify-content-between align-items-start">
+        <li class="list-group-item d-flex justify-content-between align-items-start bg-transparent text-light border-light">
             <div class="ms-2 me-auto">
-                <div class="fw-bold">${element['Directo al avion']}</div>
-                ${element['En cada ala']}
+                <div class="fw-bold">${element.fuelToPlane}</div>
+                ${element.fuelToWings}
             </div>
         </li>
     ` 
@@ -44,35 +51,27 @@ formulario.addEventListener('submit', e => {
     const calculoCombustible = diferencia / 6.7;
     const combustibleCadaAla = calculoCombustible/2
 
-    console.log(`Combustible a depositar directo al Avión: ${calculoCombustible.toFixed()}`);
-    console.log(`Combustible a depositar en cada ala: ${combustibleCadaAla.toFixed()}`);
-
-    operaciones.push(
-        {
-        'Directo al avion': `${calculoCombustible}`,
-        'En cada ala':`${combustibleCadaAla}`
-    })
+    operaciones.push( new AirpcraftFuel(Number.parseFloat(calculoCombustible).toFixed(2), Number.parseFloat(combustibleCadaAla).toFixed(2)) )
 
     resultado.innerHTML=`
     <p class="lead text-success">
         Combustible a depositar directo al Avión:
-        <span class="text-danger fw-bold">${calculoCombustible.toFixed()}gl.</span>
+        <span class="text-danger fw-bold">${Number.parseFloat(calculoCombustible).toFixed(2)}gl.</span>
     </p>
     <p class="lead text-success">
         Combustible a depositar en cada ala: 
-        <span class="text-danger fw-bold">${combustibleCadaAla.toFixed()}gl.</span>
+        <span class="text-danger fw-bold">${Number.parseFloat(combustibleCadaAla).toFixed(2)}gl.</span>
     </p>
     `
     solicitado_capitan.value=''
     contenido_avion.value=''
     
-    console.log(operaciones)
-    localStorage.setItem('db_operaciones',JSON.stringify(operaciones))
+    localStorage.setItem('db_operaciones',JSON.stringify(operaciones));
     mostrarHistorico();
 })
 
 cleanHistorial.addEventListener('click',()=>{
     localStorage.clear();
     historico.innerHTML='';
-    historial.style='display:none'
+    historial.style='display:none';
 })
